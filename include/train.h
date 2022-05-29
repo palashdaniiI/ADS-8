1c348184c8ad058825c87b11f8e0a83b7277a904
palashdaniiI <99319050+palashdaniiI@users.noreply.github.com>
2022-05-29 22:46:18 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -13,7 +13,6 @@ class Train {
   Cage *last;
   Train::Cage *create(bool light);
  public:
-  Train();
   Train(): first(nullptr), last(nullptr), countOp(0) {}
   void addCage(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
